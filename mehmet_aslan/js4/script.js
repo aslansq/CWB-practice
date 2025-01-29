294a6dd4d33208cb77400f1875f3f19d88e2b2e2
Mehmet Aslan <aaslan.mehmet@hotmail.com>
2025-01-29 18:19:40 +0100

    loop lecture part is done

---
+++ mehmet_aslan/js4/script.js
@@ -0,0 +1,60 @@
+let students = ['Peter', 'John', 'Anna'];
+
+//console.log(students[0]);
+//console.log(students[1]);
+//console.log(students[2]);
+
+//students[0] = 'peter' 
+//students[1] = 'john' 
+//students[students.length - 1] = 'anna'
+
+//console.log(students[0]);
+//console.log(students[1]);
+//console.log(students[2]);
+
+//students[0] = null; 
+//console.log(students);
+//console.log(students.length);
+
+//for(let i = 0; i < students.length; i++) {
+//    console.log(students[i]);
+//}
+
+//let i = 0
+//while(i < students.length) {
+//    console.log(students[i]);
+//    i++;
+//}
+
+
+//for (let i = 0; i < 20; i = i + 2){
+//	console.log(i);
+//}
+
+//for (let i = 0; i < 20; i++){
+//	if (i % 2 == 1){
+//		continue;
+//	}else{
+//		console.log(i);
+//	}
+//}
+
+//let s = '';
+//while (true) {
+//	s += 'a';
+//	if (s === 'aaaaa'){
+//		break;
+//	}
+//	console.log(s);
+//}
+
+//for (let i = 0; i < 5; i++) {
+//	let inner = '';
+//	for (let j = 10; j > 5; j--) {
+//		inner += j + ' ';
+//	}
+//	console.log("Outer: " + i + ", Inner:  " + inner);
+//}
+
+//Creating an Array object
+//let students = new Array('Peter', 'John', 'Anna');
